7dfceaac5cafdbce23c5a2f3e73975b107ebb2a9
Endre Márk Borza <endremborza@gmail.com>
2021-09-01 16:55:33 +0200

    set constants

---
+++ sscutils/constants.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+SUBSET_CONFIG_PATH = Path("subsets.yaml")
+IMPORTED_DATASETS_CONFIG_PATH = Path("datasets.yaml")
+ENVIRONMENT_CONFIG_PATH = Path("project-env.yaml")
+PARAMS_PATH = Path("params.yaml")
+
+DATA_PATH = Path("data")
+SRC_PATH = Path("src")
+DATA_MANAGEMENT_SRC_PATH = Path(SRC_PATH, "data_management")
+
+RAW_COLS_PYFILENAME = "raw_cols.py"
+TREPOS_PYFILENAME = "trepos.py"
