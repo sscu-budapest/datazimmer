8adb2f54f16157d9db6f8c6bce098f1e2eebfff0
Endre Márk Borza <endremborza@gmail.com>
2021-10-07 00:40:19 +0200

    iterate once on metadata schema

--- sscutils/metadata/schema.py
+++ sscutils/metadata/schema.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Union
 
 
 class DType(Enum):
+    # TODO: categorical
     float = float
     int = int
     str = str
@@ -22,35 +23,45 @@ class Column:
 
 @dataclass
 class ForeignKey:
-    name: str
-    dtype: DType
-    subject: str
+    table: str  # an ID, that needs to link to a table
+    prefix: str
     description: Optional[str] = None
 
 
 @dataclass
 class ColumnGroupInstance:
-    group_name: str
-    instance_name: str
-    description: Optional[str]
+    group_name: str  # an ID, that needs to link to a group
+    prefix: str
+    description: Optional[str] = None
 
 
-ANY_COL_TYPE = Union[Column, ColumnGroupInstance, ForeignKey]
+ANY_COL_ROOT_TYPE = Union[Column, ColumnGroupInstance, ForeignKey]
 
 
 @dataclass
 class ColumnGroup:
     name: str
-    columns: List[ANY_COL_TYPE]
+    columns: List[ANY_COL_ROOT_TYPE]
     description: Optional[str] = None
 
 
 @dataclass
 class Table:
+    """TODO: document restrictions here
+
+    or find new location for them
+
+    e. g.
+    - no column group instances with same prefix in same table
+    - column group needs to be found from instance
+    - column name cant contain __ (and other)
+
+    """
+
     name: str
-    columns: List[ANY_COL_TYPE]
+    columns: List[ANY_COL_ROOT_TYPE]
     subject_of_records: str
-    index_cols: Optional[List[ANY_COL_TYPE]] = None
-    partitioning_cols: Optional[List[ANY_COL_TYPE]] = None
+    index_cols: Optional[List[ANY_COL_ROOT_TYPE]] = None
+    partitioning_cols: Optional[List[ANY_COL_ROOT_TYPE]] = None
     partition_max_rows: Optional[int] = None
     description: Optional[str] = None
