d92ab35bbef96fa02794507230c06f8e8adbafd0
Endre Márk Borza <endremborza@gmail.com>
2021-10-13 15:33:59 +0200

    cleanup some mock artifact files

--- mock-artifacts/dataset1/src/trepos.py
+++
@@ -1,5 +0,0 @@
-from sscutils import create_trepo_with_subsets
-
-persons_table = create_trepo_with_subsets("persons")
-objects_table = create_trepo_with_subsets("objects")
-events_table = create_trepo_with_subsets("events")
--- mock-artifacts/dataset2/src/trepos.py
+++
@@ -1,3 +0,0 @@
-from sscutils import create_trepo_with_subsets
-
-rules_table = create_trepo_with_subsets("rules")
