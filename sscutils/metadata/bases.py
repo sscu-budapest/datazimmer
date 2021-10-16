2a7a0e79ebde6881b149c731e35779c664e3c9cb
Endre Márk Borza <endremborza@gmail.com>
2021-10-16 18:51:10 +0200

    first draft of metadata bases

---
+++ sscutils/metadata/bases.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from colassigner import ColAccessor
+
+
+class EntityClassBase:
+    pass
+
+
+class IndexBase(ColAccessor):
+    pass
+
+
+class CompositeTypeBase(ColAccessor):
+    pass
+
+
+class TableFeaturesBase(ColAccessor):
+    pass
+
+
+def get_feature_dict(cls: Union[CompositeTypeBase, TableFeaturesBase]):
+    return {k: v for k, v in cls.__dict__.items() if not k.startswith("_")}
