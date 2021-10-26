db34a16b2efef067f9dec1311bbe86c4ade00707
Endre Márk Borza <endremborza@gmail.com>
2021-10-26 22:11:17 +0200

    import ns

--- dogshow/artifacts/cc-project-a/{{cookiecutter.artifact}}/src/base_report/make_report.py
+++ dogshow/artifacts/cc-project-a/{{cookiecutter.artifact}}/src/base_report/make_report.py
@@ -1,12 +1,14 @@
-from ..success import sized_dogs_table, DogSizeCalculation
+from .. import success
 from .report_meta import size_count_table, SizeCountFeatures
 from ..pipereg import pipereg
 
 
-@pipereg.register(dependencies=[sized_dogs_table], outputs=[size_count_table])
+@pipereg.register(
+    dependencies=[success.sized_dogs_table], outputs=[size_count_table]
+)
 def report_step():
-    dog_w_size_df = sized_dogs_table.get_full_df()
+    dog_w_size_df = success.sized_dogs_table.get_full_df()
 
     dog_w_size_df.assign(**{SizeCountFeatures.count: 1}).groupby(
-        DogSizeCalculation.size
+        success.DogSizeCalculation.size
     ).sum().pipe(size_count_table.trepo.replace_all)
