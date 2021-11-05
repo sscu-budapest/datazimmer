fac0dab42ed71d70b2624b1d470a3e79b5501406
Endre Márk Borza <endremborza@gmail.com>
2021-11-05 14:06:17 +0100

    fix tests

--- sscutils/tests/test_util.py
+++ sscutils/tests/test_util.py
@@ -2,7 +2,7 @@ from sscutils.utils import format_code
 
 
 def test_lint():
-    bad_c = "x = 10"
-    good_c = "x =  10"
+    bad_c = "x =  10"
+    good_c = "x = 10\n"
     assert good_c == format_code(bad_c)
     assert good_c == format_code(good_c)
