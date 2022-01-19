2e7e47ba261ef75bb9749f2c852f777be811c85a
Endre Márk Borza <endremborza@gmail.com>
2022-01-19 16:36:50 +0100

    expose run_step

--- sscutils/__init__.py
+++ sscutils/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from ._version import __version__
 from .artifact_context import dump_dfs_to_tables
+from .helpers import run_step
 from .invoke_commands import dataset_ns, project_ns
 from .metadata.datascript import (
     BaseEntity,
