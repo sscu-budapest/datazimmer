7951646832f996025cc9beaefd5ef6ad5bfb6f50
Endre Márk Borza <endremborza@gmail.com>
2022-03-20 12:27:11 +0100

    redo bedrock

--- sscutils/metadata/bedrock/conversion.py
+++ sscutils/metadata/bedrock/conversion.py
@@ -1,33 +1,34 @@
 from dataclasses import dataclass
 from functools import partial
-from typing import Callable, List, Optional
+from typing import TYPE_CHECKING, Callable, List, Optional
 
 from colassigner.constants import PREFIX_SEP
 
 from ...utils import chainmap
-from .artifact_metadata import ArtifactMetadata
-from .column import Column
+from .column import Column, to_dt_map
+from .complete_id import CompleteId, CompleteIdBase
 from .feature_types import CompositeFeature, ForeignKey, PrimitiveFeature
-from .namespace_metadata import NamespaceMetadata
-from .namespaced_id import NamespacedId
+
+if TYPE_CHECKING:
+    from ...artifact_context import ArtifactContext  # pragma: no cover
 
 
 @dataclass
 class FeatConverter:
 
-    ns_meta: NamespaceMetadata
-    a_meta: ArtifactMetadata
+    runtime: "ArtifactContext"
+    init_base: CompleteIdBase
     wrapper: Callable = lambda x: x
     proc_fk: Optional[Callable] = None
 
     def feats_to_cols(self, feats) -> List[Column]:
-        return chainmap(self.feat_to_cols, feats)
+        return chainmap(partial(self.feat_to_cols, calling_base=self.init_base), feats)
 
     def feat_to_cols(
         self,
         feat,
+        calling_base,
         init_prefix=(),
-        calling_ns_prefix=None,
         open_to_fk=True,
     ) -> List[Column]:
 
@@ -38,36 +39,46 @@ class FeatConverter:
             return [self.wrapper(Column(name, feat.dtype, feat.nullable))]
         if isinstance(feat, CompositeFeature):
             sub_id = feat.dtype
-            subfeats = self._get_atom(sub_id, calling_ns_prefix).features
+            subfeats = self._get_atom(sub_id, calling_base).features
         elif isinstance(feat, ForeignKey):
             new_open_to_fk = False
             sub_id = feat.table
-            fk_to = self._get_id(sub_id, calling_ns_prefix)
-            table_obj = self._get_atom(sub_id, calling_ns_prefix)
+            fk_to = self._get_id(sub_id, calling_base)
+            table_obj = self._get_atom(sub_id, calling_base)
             subfeats = table_obj.index
 
-        new_ns_prefix = (
-            sub_id.ns_prefix if not sub_id.is_local else calling_ns_prefix
-        )
+        new_base = sub_id.base if not sub_id.is_local else calling_base
         new_feat_prefix = (*init_prefix, feat.prefix)
         new_fun = partial(
             self.feat_to_cols,
             init_prefix=new_feat_prefix,
-            calling_ns_prefix=new_ns_prefix,
+            calling_base=new_base,
             open_to_fk=new_open_to_fk,
         )
         out = chainmap(new_fun, subfeats)
         if fk_to is not None and open_to_fk and self.proc_fk:
-            self.proc_fk(out, fk_to, new_feat_prefix)
+            self.proc_fk(out, fk_to.absolute_from(calling_base), new_feat_prefix)
 
         return out
 
-    def _get_id(self, id_: NamespacedId, calling_namespace=None):
-        if not id_.is_local:
-            return id_
-        if calling_namespace is None:
-            calling_namespace = self.ns_meta.local_name
-        return NamespacedId(calling_namespace, id_.obj_id)
+    def _get_id(self, id_: CompleteId, calling_base: CompleteIdBase):
+        if id_.is_local:
+            return calling_base.to_id(id_.obj_id)
+        return id_
 
-    def _get_atom(self, id_: NamespacedId, calling_namespace=None):
-        return self.a_meta.get_atom(self._get_id(id_, calling_namespace))
+    def _get_atom(self, id_: CompleteId, calling_base: CompleteIdBase):
+        return self.runtime.get_atom(self._get_id(id_, calling_base))
+
+
+def table_id_to_dtype_maps(id_: CompleteId, runtime: "ArtifactContext"):
+    table = runtime.get_atom(id_)
+    convfun = FeatConverter(runtime, id_.base).feats_to_cols
+    return map(
+        lambda feats: to_dt_map(convfun(feats)),
+        [table.features, table.index],
+    )
+
+
+def table_id_to_dtype_map(id_: CompleteId, runtime):
+    feat_dic, ind_dic = table_id_to_dtype_maps(id_, runtime)
+    return {**feat_dic, **ind_dic}
--- sscutils/metadata/bedrock/namespace_metadata.py
+++ sscutils/metadata/bedrock/namespace_metadata.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
+from pathlib import Path
 from typing import List
 
-from ...naming import ROOT_NS_LOCAL_NAME, NamespaceMetadataPaths
+from ...naming import SDistPaths
 from .atoms import NS_ATOM_TYPE, CompositeType, EntityClass, Table
-from .feature_types import ANY_FEATURE_TYPE
 from .load_util import dump_atom_list_to_dict, load_atom_dict_to_list
 
 
@@ -14,7 +14,7 @@ class NamespaceMetadata:
     composite_types: List[CompositeType]
     entity_classes: List[EntityClass]
     tables: List[Table]
-    local_name: str
+    name: str
 
     def get(self, obj_id: str) -> NS_ATOM_TYPE:
         for atom in self.atoms:
@@ -23,48 +23,25 @@ class NamespaceMetadata:
         raise KeyError(f"{obj_id} not found in: \n {self}")
 
     @classmethod
-    def load_serialized(cls, subdir=ROOT_NS_LOCAL_NAME) -> "NamespaceMetadata":
-        ns_paths = NamespaceMetadataPaths(subdir)
+    def load_serialized(cls, subdir: Path) -> "NamespaceMetadata":
+        ns_paths = SDistPaths(subdir)
         return cls(
             composite_types=load_atom_dict_to_list(
                 ns_paths.composite_types, CompositeType
             ),
-            entity_classes=load_atom_dict_to_list(
-                ns_paths.entity_classes, EntityClass
-            ),
+            entity_classes=load_atom_dict_to_list(ns_paths.entity_classes, EntityClass),
             tables=load_atom_dict_to_list(ns_paths.table_schemas, Table),
-            local_name=subdir,
+            name=subdir.name,
         )
 
-    def dump(self):
-        ns_paths = NamespaceMetadataPaths(self.local_name, mkdir=True)
+    def dump(self, path):
+        ns_paths = SDistPaths(path)
         for path, atom_list in [
             (ns_paths.table_schemas, self.tables),
             (ns_paths.entity_classes, self.entity_classes),
             (ns_paths.composite_types, self.composite_types),
         ]:
             dump_atom_list_to_dict(path, atom_list)
-
-    @property
-    def used_prefixes(self) -> List[str]:
-        full_feature_list: List[ANY_FEATURE_TYPE] = []
-        ns_ids = []
-        for table in self.tables:
-            full_feature_list += table.features_w_ind
-            ns_ids.append(table.subject_of_records.ns_prefix)
-
-        for ct in self.composite_types:
-            full_feature_list += ct.features
-
-        for ec in self.entity_classes:
-            for ecp in ec.parents:
-                ns_ids.append(ecp.ns_prefix)
-
-        full_ns_id_list = [
-            feat.val_id.ns_prefix for feat in full_feature_list
-        ] + ns_ids
-
-        return list(set(filter(None, full_ns_id_list)))
 
     @property
     def atoms(self) -> List[NS_ATOM_TYPE]:
