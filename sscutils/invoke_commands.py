bba23df58b82e7f0817b80b804ac3434ee3ee0f6
Endre Márk Borza <endremborza@gmail.com>
2022-03-20 12:36:41 +0100

    reshape inv

--- sscutils/invoke_commands.py
+++ sscutils/invoke_commands.py
@@ -2,111 +2,61 @@ from dataclasses import asdict
 from shutil import rmtree
 
 from dvc.repo import Repo
-from invoke import Collection, task
-from invoke.exceptions import UnexpectedExit
+from invoke import Collection, UnexpectedExit, task
 from structlog import get_logger
 
-from .artifact_context import ArtifactContext
-from .config_loading import DatasetConfig, RunConfig
-from .helpers import (
-    import_env_creator_function,
-    import_pipereg,
-    import_update_data_function,
-)
-from .metadata.datascript.conversion import (
-    all_datascript_to_bedrock,
-    imported_bedrock_to_datascript,
-)
-from .naming import (
-    COMPLETE_ENV_NAME,
-    IMPORTED_NAMESPACES_SCRIPTS_PATH,
-    METADATA_DIR,
-    SRC_PATH,
-)
+from .config_loading import Config, RunConfig, get_tag
+from .get_runtime import get_runtime
+from .naming import CONSTR, MAIN_MODULE_NAME
+from .pipeline_registry import get_global_pipereg
+from .registry import Registry
 from .utils import LINE_LEN
-from .validation_functions import validate_dataset, validate_project
+from .validation_functions import validate_artifact
 
 logger = get_logger(ctx="invoke task")
 
 
 @task
 def lint(ctx, line_length=LINE_LEN):
-    ctx.run(f"black {SRC_PATH} -l {line_length}")
-    ctx.run(f"isort {SRC_PATH} --profile black")
-    ctx.run(f"flake8 {SRC_PATH} --max-line-length={line_length}")
+    ctx.run(f"black {MAIN_MODULE_NAME} -l {line_length}")
+    ctx.run(f"isort {MAIN_MODULE_NAME} --profile black")
+    ctx.run(f"flake8 {MAIN_MODULE_NAME} --max-line-length={line_length}")
 
 
 @task
-def set_dvc_remotes(ctx):
-    for branch, remote in ArtifactContext().branch_remote_pairs:
-        _try_checkout(ctx, branch)
-        ctx.run(f"dvc remote default {remote}")
-        ctx.run("git add .dvc")
-        ctx.run(f'git commit -m "update dvc default remote to {remote}"')
+def release(ctx, constr=CONSTR):
+    # TODO
+    # check if version tag is already present in remote registry
+    # check dvc remotes from created-envs are present
+    # validate dataset
+    # tag first
+    # try it all, push tags if good, remove tags if bad
+    # need to push to dvc though!
+    # for the switching of dvc repos, prep work needs to be done
+
+    runtime = get_runtime(True)
+    assert runtime.config.validation_envs
+    dvc_repo = Repo()
+    run(ctx)  # TODO: nothing should run (?)
+    # avoid tagging the same data differently
+    data_version = runtime.metadata.next_data_v
+    for env in runtime.config.sorted_envs:
+        targets = runtime.pipereg.step_names_of_env(env.name)
+        logger.info("pushing targets", targets=targets, env=env.name)
+        dvc_repo.push(targets=targets, remote=env.remote)
+        vtag = get_tag(runtime.config.version, data_version, env.name)
+        _commit_dvc_default(ctx, env.remote)
+        ctx.run(f"git tag {vtag}")
+        ctx.run(f"git push origin {vtag}")
+    ctx.run("git push")
+    runtime.registry.publish()
+    validate(ctx, con=constr)
 
 
-@task
-def import_namespaces(
-    ctx, to_datascript=True, git_commit=False, overwrite=True
-):
-    ArtifactContext().import_namespaces(overwrite)
-    if to_datascript:
-        imported_bedrock_to_datascript()
-    if not git_commit:
-        return
-
-    try:
-        _commit_serialized_meta(ctx)
-        ctx.run(f"git add {IMPORTED_NAMESPACES_SCRIPTS_PATH}")
-        ctx.run('git commit -m "import namespaces to datascript"')
-    except UnexpectedExit:
-        pass
-
-
-@task(iterable=["args"])
-def update_data(_, args=None):
-    pargs = args or ()
-    import_update_data_function()(*pargs)
-
-
-@task
-def write_envs(_):
-    dataset_config = DatasetConfig()
-    def_env = dataset_config.default_env
-    err_str = f"{def_env} env needs to exist before creating others"
-    assert def_env.path.exists(), err_str
-    env_creator_fun = import_env_creator_function()
-    for env in dataset_config.created_environments:
-        if env.name == COMPLETE_ENV_NAME:
-            continue
-        env_creator_fun(env.name, **env.kwargs)
-
-
-@task
-def push_envs(ctx, git_push=False):
-    dataset_config = DatasetConfig()
-    for env in dataset_config.created_environments:
-        _try_checkout(ctx, env.branch)
-        dvc_repo = Repo()
-        dvc_repo.add(env.posix)
-        ctx.run(f"git add {env.posix}.dvc **/.gitignore")
-        try:
-            ctx.run(f'git commit -m "add data environment {env.name}"')
-        except UnexpectedExit:  # pragma: no cover
-            continue
-        if git_push:
-            try:
-                ctx.run("git push")
-            except UnexpectedExit:
-                ctx.run(f"git push -u origin {env.branch}")
-        dvc_repo.push()
-
-
-@task(aliases=("sdm",))
-def serialize_datascript_metadata(ctx, git_commit=False):
-    all_datascript_to_bedrock()
-    if git_commit:
-        _commit_serialized_meta(ctx)
+@task(aliases=("build",))
+def build_meta(_):
+    get_global_pipereg(reset=True)
+    Registry(Config.load()).full_build()
 
 
 @task
@@ -116,39 +66,46 @@ def load_external_data(ctx, git_commit=False):
     should probably be a bit more clever,
     but dvc handles quite a lot of caching
     """
-
+    runtime = get_runtime(True)
     dvc_repo = Repo()
-    for ns_env in ArtifactContext().data_envs:
-        rmtree(ns_env.out_path, ignore_errors=True)  # brave thing...
-        ns_env.out_path.parent.mkdir(exist_ok=True, parents=True)
-        ns_env.load_data(dvc_repo)
+    logger.info("loading external data", envs=runtime.data_to_load)
+    for data_env in runtime.data_to_load:
+        rmtree(data_env.path, ignore_errors=True)  # brave thing...
+        data_env.path.parent.mkdir(exist_ok=True, parents=True)
+        data_env.load_data(dvc_repo)
         if git_commit:
-            ctx.run(f"git add *.gitignore {ns_env.out_posix}.dvc")
+            ctx.run(f"git add *.gitignore {data_env.posix}.dvc")
             ctx.run(
                 'git commit -m "add imported dataset'
-                f' {ns_env.local_name}: {ns_env.env}"'
+                f' {data_env.posix}: {data_env.env}"'
             )
 
 
 @task
-def validate(_, env=None, con="sqlite:///:memory:", draw=False, batch=20000):
-    artifact_ctx = ArtifactContext()
-    if artifact_ctx.is_dataset:
-        validate_dataset(con, env, draw, batch_size=batch)
-    else:
-        validate_project()
+def validate(_, con=CONSTR, draw=False, batch=20000):
+    validate_artifact(con, draw, batch)
 
 
 @task
-def run(_, stage=True, profile=False, force=False):
+def cleanup(ctx):
+    conf = Config.load()
+    aname = conf.name
+    reg = Registry(conf, True)
+    ctx.run(f"pip uninstall {aname} -y")
+    reg.purge()
+
+
+@task
+def run(_, stage=True, profile=False, force=False, env=None):
     conf = {}
     if stage:
         conf["core"] = {"autostage": True}
     dvc_repo = Repo(config=conf)
 
-    pipereg = import_pipereg()
+    runtime = get_runtime()
+
     stage_names = []
-    for step in pipereg.steps:
+    for step in runtime.pipereg.steps:
         logger.info("adding step", step=step)
         step.add_as_stage(dvc_repo)
         stage_names.append(step.name)
@@ -165,31 +122,21 @@ def run(_, stage=True, profile=False, force=False):
         stage.remove_outs(force=True)
         dvc_repo.lock.unlock()
 
-    conf = RunConfig(profile=profile)
-    logger.info("running repro", **asdict(conf))
-    with conf:
-        dvc_repo.reproduce(force=force)
+    for env in runtime.config.envs:
+        rconf = RunConfig(write_env=env.name, profile=profile)
+        with rconf:
+            env_stages = runtime.pipereg.step_names_of_env(env.name)
+            logger.info("running repro", stages=env_stages, **asdict(rconf))
+            dvc_repo.reproduce(force=force, targets=env_stages)
 
 
-common_tasks = [
-    lint,
-    set_dvc_remotes,
-    import_namespaces,
-    serialize_datascript_metadata,
-    validate,
-]
-
-dataset_ns = Collection(*common_tasks, update_data, write_envs, push_envs)
-project_ns = Collection(*common_tasks, load_external_data, run)
+ns = Collection(*[lint, release, build_meta, validate, load_external_data, run])
 
 
-def _try_checkout(ctx, branch):
+def _commit_dvc_default(ctx, remote):
+    ctx.run(f"dvc remote default {remote}")
+    ctx.run("git add .dvc")
     try:
-        ctx.run(f"git checkout {branch}")
+        ctx.run(f'git commit -m "update dvc default remote to {remote}"')
     except UnexpectedExit:
-        ctx.run(f"git checkout -b {branch}")
-
-
-def _commit_serialized_meta(ctx):
-    ctx.run(f"git add {METADATA_DIR}")
-    ctx.run('git commit -m "update serialized metadata"')
+        pass
