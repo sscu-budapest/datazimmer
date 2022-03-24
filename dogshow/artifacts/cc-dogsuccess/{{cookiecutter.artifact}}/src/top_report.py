07deadc144340008f28f0523e4319ad9d56d9604
Endre Márk Borza <endremborza@gmail.com>
2022-03-24 16:45:11 +0100

    ns change in version

--- dogshow/artifacts/cc-dogsuccess/{{cookiecutter.artifact}}/src/top_report.py
+++ dogshow/artifacts/cc-dogsuccess/{{cookiecutter.artifact}}/src/top_report.py
@@ -4,15 +4,10 @@ from .counts.meta import SizeCountFeatures, size_count_table
 from .sex_matches import SexMatchFeatures, sex_match_table
 
 top_tables_report = dz.ReportFile("tops.md")
+deps = [sex_match_table, size_count_table]
 
 
-@dz.register(
-    dependencies=[
-        sex_match_table,
-        size_count_table,
-    ],
-    outputs_nocache=[top_tables_report],
-)
+@dz.register(dependencies=deps, outputs_nocache=[top_tables_report])  # remove in v1.0
 def calculate_sex_match(show_top):
 
     sm_df = sex_match_table.get_full_df().sort_values(
