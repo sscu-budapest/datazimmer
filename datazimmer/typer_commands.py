ec26bf3a2dd65eeed8c4f8bad65d0fa06c26f0ca
Endre Márk Borza <endremborza@gmail.com>
2022-04-26 02:04:43 +0200

    use env str...

--- datazimmer/typer_commands.py
+++ datazimmer/typer_commands.py
@@ -145,7 +145,7 @@ def run(
         dvc_repo.lock.lock()
         stage.remove_outs(force=True)
         dvc_repo.lock.unlock()
-    targets = runtime.pipereg.step_names_of_env(env.name) if env else None
+    targets = runtime.pipereg.step_names_of_env(env) if env else None
     rconf = RunConfig(profile=profile)
     with rconf:
         logger.info("running repro", targets=targets, **asdict(rconf))
