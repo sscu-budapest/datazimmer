0e59e4bbaa918aea6e36b620ba26eb2446651c87
Endre Márk Borza <endremborza@gmail.com>
2022-10-22 16:51:01 +0200

    rm fstr

--- datazimmer/tests/test_registry.py
+++ datazimmer/tests/test_registry.py
@@ -7,6 +7,6 @@ from datazimmer.registry import _de_auth
 def test_de_auth():
     os.environ[GIT_TOKEN_ENV_VAR] = "GTOKEN"
     url_auth = "git@github.com:sscu-budapest/datazimmer.git"
-    url_tokened = f"https://GTOKEN@github.com/sscu-budapest/datazimmer"
+    url_tokened = "https://GTOKEN@github.com/sscu-budapest/datazimmer"
     assert _de_auth(url_auth) == "https://github.com/sscu-budapest/datazimmer"
     assert _de_auth(url_auth, re_auth=True) == url_tokened
