dab367f4916ba47f61722f54069e84d8f618506d
Endre Márk Borza <endremborza@gmail.com>
2022-11-14 11:24:49 +0100

    dependency independence

--- datazimmer/aswan_integration.py
+++ datazimmer/aswan_integration.py
@@ -1,17 +1,21 @@
-from aswan import ANY_HANDLER_T, Project
+from typing import TYPE_CHECKING
 
 from .config_loading import Config
 from .metadata.complete_id import CompleteIdBase
 from .utils import get_creation_module_name
 
+if TYPE_CHECKING:
+    from aswan import ANY_HANDLER_T
+
 
 # TODO: add just checking on and logging external project (depot)
 class DzAswan:
     name: str = None
-    starters: dict[type[ANY_HANDLER_T], list[str]] = {}
+    starters: dict[type["ANY_HANDLER_T"], list[str]] = {}
     cron: str = ""
 
     def __init__(self, global_run=False) -> None:
+        from aswan import Project
 
         self._module = get_creation_module_name()
         self._ns = global_run or CompleteIdBase.from_module_name(self._module).namespace
@@ -28,13 +32,13 @@ class DzAswan:
         self._conf.update_aswan_spec(self.name, latest.name)
         self._project.depot.current.purge()
 
-    def get_unprocessed_events(self, handler: ANY_HANDLER_T):
+    def get_unprocessed_events(self, handler: "ANY_HANDLER_T"):
         spec = self._conf.get_aswan_spec(self.name)
         from_status = spec.processed_upto_by_ns.get(self._ns)
         self._project.depot.pull(post_status=from_status)
         return self._project.depot.get_handler_events(handler)
 
-    def get_all_events(self, handler: ANY_HANDLER_T):
+    def get_all_events(self, handler: "ANY_HANDLER_T"):
         self._project.depot.pull(complete=True)
         return self._project.depot.get_handler_events(handler)
 
