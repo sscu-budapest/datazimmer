3b3da3fd14b5ecd23973eb202bd26721e239d310
Endre Márk Borza <endremborza@gmail.com>
2022-11-16 19:18:08 +0100

    improve aswan integration

--- datazimmer/aswan_integration.py
+++ datazimmer/aswan_integration.py
@@ -21,27 +21,34 @@ class DzAswan:
         self._ns = global_run or CompleteIdBase.from_module_name(self._module).namespace
         self._conf = Config.load()
         self._project = Project(self.name)
-        self._project.depot.setup()
+        self._depot = self._project.depot
+        self._depot.setup()
 
     def run(self):
         self.extend_starters()
-        self._project.depot.pull()
+        self._depot.pull()
         self._project.cleanup_current_run()
         self._project.run(urls_to_overwrite=self.starters)
-        latest = self._project.depot.save_current()
-        self._project.depot.push()
+        latest = self._depot.save_current()
+        self._depot.push()
         self._conf.update_aswan_spec(self.name, latest.name)
-        self._project.depot.current.purge()
+        self._depot.current.purge()
 
-    def get_unprocessed_events(self, handler: "ANY_HANDLER_T"):
+    def get_unprocessed_events(self, handler: "ANY_HANDLER_T", only_latest=False):
         spec = self._conf.get_aswan_spec(self.name)
         from_status = spec.processed_upto_by_ns.get(self._ns)
-        self._project.depot.pull(post_status=from_status)
-        return self._project.depot.get_handler_events(handler)
+        self._depot.pull(post_status=from_status)
+        if from_status:
+            runs = self._depot.get_missing_runs(self._depot.get_status(from_status))
+        else:
+            runs = None
+        return self._depot.get_handler_events(
+            handler, only_latest=only_latest, past_runs=runs
+        )
 
     def get_all_events(self, handler: "ANY_HANDLER_T"):
-        self._project.depot.pull(complete=True)
-        return self._project.depot.get_handler_events(handler)
+        self._depot.pull(complete=True)
+        return self._depot.get_handler_events(handler)
 
     def extend_starters(self):
         """this runs prior to running the project"""
