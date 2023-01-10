33b3c0b87509dbb38143fc55b2a644cc553bfd3d
Endre Márk Borza <endremborza@gmail.com>
2023-01-10 18:22:42 +0100

    bump v and expose

--- datazimmer/__init__.py
+++ datazimmer/__init__.py
@@ -13,7 +13,8 @@ from .metadata.scrutable import ScruTable
 from .persistent_state import PersistentState
 from .pipeline_element import register, register_data_loader, register_env_creator
 from .project_runtime import dump_dfs_to_tables
+from .raw_data import get_raw_data_path
 from .reporting import ReportFile
 from .typer_commands import app
 
-__version__ = "0.4.6"
+__version__ = "0.4.7"
