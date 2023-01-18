ba7596302a5fd8fdafcbce1199033912872ad36f
Endre Márk Borza <endremborza@gmail.com>
2023-01-18 16:22:48 +0100

    some new some discarded commands

--- datazimmer/typer_commands.py
+++ datazimmer/typer_commands.py
@@ -1,25 +1,37 @@
 import datetime as dt
 import re
 from dataclasses import asdict
+from pathlib import Path
 from subprocess import check_call
 
 import typer
 from dvc.repo import Repo
 from structlog import get_logger
 
-from .config_loading import CONF_KEYS, Config, RunConfig, get_tag, meta_version_from_tag
+from .config_loading import CONF_KEYS, Config, RunConfig, UserConfig
 from .exceptions import ProjectSetupException
 from .explorer import build_explorer, init_explorer, load_explorer_data
 from .get_runtime import get_runtime
 from .gh_actions import write_aswan_crons, write_project_cron
 from .metadata.high_level import ProjectMetadata
-from .naming import BASE_CONF_PATH, MAIN_MODULE_NAME, SANDBOX_DIR, TEMPLATE_REPO
+from .naming import (
+    BASE_CONF_PATH,
+    MAIN_MODULE_NAME,
+    README_PATH,
+    SANDBOX_DIR,
+    TEMPLATE_REPO,
+    VERSION_PREFIX,
+    env_from_tag,
+    get_tag,
+    meta_version_from_tag,
+)
 from .raw_data import IMPORTED_RAW_DATA_DIR, RAW_DATA_DIR, RAW_ENV_NAME
 from .registry import Registry
 from .sql.draw import dump_graph
 from .sql.loader import tmp_constr
-from .utils import gen_rmtree, get_git_diffs, git_run
+from .utils import command_out_w_prefix, gen_rmtree, get_git_diffs, git_run
 from .validation_functions import validate
+from .zenodo import CITATION_FILE, ZenApi
 
 logger = get_logger(ctx="CLI command")
 app = typer.Typer()
@@ -36,36 +48,35 @@ def run_step(name: str, env: str):
 
 
 @app.command()
-def publish_meta():
-    _validate_empty_vc("publishing meta", [MAIN_MODULE_NAME])
-    get_runtime().registry.publish()
-
-
-@app.command()
-def update_registry():
-    Registry(Config.load()).update()
-
-
-@app.command()
 def init(name: str):
     git_run(clone=(TEMPLATE_REPO, name), depth=None)
     c_p = name / BASE_CONF_PATH
     cstr = re.sub(f"{CONF_KEYS.name}: .+", f"{CONF_KEYS.name}: {name}", c_p.read_text())
     c_p.write_text(cstr)
+    rm_p = name / README_PATH
+    rm_p.write_text(rm_p.read_text().replace("{{title}}", name))
     check_call(["git", "remote", "rm", "origin"], cwd=name)
 
 
 @app.command()
-def update():
-    update_registry()
-    git_run(pull=True)
-    Repo().pull()
+def update(registry: bool = True, code: bool = True, dvc: bool = True):
+    if registry:
+        Registry(Config.load()).update()
+    if code:
+        git_run(pull=True)
+    if dvc:
+        Repo().pull()
 
 
 @app.command()
 def draw(v: bool = False):
     with tmp_constr(v) as constr:
         dump_graph(constr)
+
+
+@app.command()
+def set_whoami(first_name: str, last_name: str, orcid: str):
+    UserConfig(first_name, last_name, orcid).dump()
 
 
 @app.command()
@@ -86,6 +97,7 @@ def import_raw(project: str, tag: str = ""):
 @app.command()
 def publish_data():
     build_meta()
+    # TODO: ensure that this build gets published
     _validate_empty_vc("publishing data")
     runtime = get_runtime()
     dvc_repo = Repo()
@@ -113,6 +125,41 @@ def publish_data():
     for tag_to_push in vtags:
         check_call(["git", "push", "origin", tag_to_push])
     runtime.registry.publish()
+
+
+@app.command()
+def publish_to_zenodo(env: str = "", test: bool = False):
+    # must be at a zimmer tag
+    _validate_empty_vc("publishing to zenodo")
+    runtime = get_runtime()
+    tag_env, tag = _get_current_tag_of_env(env)
+    if tag is None:
+        if not env:
+            raise ProjectSetupException(
+                "cant publish untagged commit. "
+                f"either checkout a published tag or run {publish_data}"
+            )
+        else:
+            raise ValueError(f"can't find {env} amond tags of HEAD")
+
+    zapi = ZenApi(runtime.config, test=test, tag=tag)
+    did = zapi.get_depo_id()
+    if tag_env == RAW_ENV_NAME:
+        zapi.upload_directory(RAW_DATA_DIR, depo_id=did)
+    else:
+        dvc_repo = Repo()
+        for step in runtime.step_names_of_env(tag_env):
+            for out in dvc_repo.index[step].outs:
+                op = Path(out.fs_path)
+                if op.is_absolute():
+                    op = op.relative_to(Path.cwd())
+                if op.is_dir():
+                    zapi.upload_directory(op, depo_id=did)
+                elif op.exists():
+                    zapi.upload_file(op, depo_id=did)
+    zapi.publish(zid=did)
+    zapi.update_readme(depo_id=did)
+    git_run(add=[README_PATH, CITATION_FILE], msg=f"doi for {tag}", check=True)
 
 
 @app.command()
@@ -191,12 +238,22 @@ def run(
     return runs
 
 
+def _get_current_tag_of_env(env: str):
+    tag_comm = ["git", "tag", "--points-at", "HEAD"]
+    for tag in command_out_w_prefix(tag_comm, VERSION_PREFIX):
+        tag_env = env_from_tag(tag)
+        if (env == tag_env) or not env:
+            return tag_env, tag
+    return None, None
+
+
 def _commit_dvc_default(remote):
     check_call(["dvc", "remote", "default", remote])
     git_run(add=[".dvc"], msg=f"update dvc default remote to {remote}", check=True)
 
 
 def _validate_empty_vc(attempt, prefs=("dvc.", MAIN_MODULE_NAME)):
+    # TODO: in case of raw data, might need to check notebooks
     for fp in get_git_diffs() + get_git_diffs(True):
         if any([*[fp.startswith(pref) for pref in prefs], fp == BASE_CONF_PATH]):
             msg = f"{fp} should be committed to git before {attempt}"
