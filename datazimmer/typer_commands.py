5ad07564bc4f49860cd7cc1d37559f9074c90a09
Endre Márk Borza <endremborza@gmail.com>
2023-01-22 15:44:22 +0100

    align hopefully for the last time with new dvc

--- datazimmer/typer_commands.py
+++ datazimmer/typer_commands.py
@@ -168,7 +168,7 @@ def publish_to_zenodo(env: str = "", test: bool = False):
     else:
         dvc_repo = Repo()
         for step in runtime.step_names_of_env(tag_env):
-            for out in dvc_repo.index.stage_collector.get_target(step).outs:
+            for out in list(dvc_repo.stage.collect(step))[0].outs:
                 op = Path(out.fs_path)
                 if op.is_absolute():
                     op = op.relative_to(Path.cwd())
