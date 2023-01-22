ba37b8eaeeee0462d50eb71669b8448a282db6a6
Endre Márk Borza <endremborza@gmail.com>
2023-01-22 15:21:24 +0100

    move some meta around

--- datazimmer/metadata/atoms.py
+++ datazimmer/metadata/atoms.py
@@ -1,8 +1,14 @@
 from abc import abstractmethod
 from dataclasses import dataclass, field
-from typing import List, Optional, Union
+from functools import partial
+from typing import List, Optional, TypeVar, Union
 
-from ..utils import PRIMITIVE_MODULES, get_simplified_mro
+import pandas as pd
+import sqlalchemy as sa
+from colassigner.constants import PREFIX_SEP
+from structlog import get_logger
+
+from ..utils import PRIMITIVE_MODULES, chainmap, get_simplified_mro
 from .datascript import (
     AbstractEntity,
     CompositeTypeBase,
@@ -10,9 +16,14 @@ from .datascript import (
     Nullable,
     PrimitiveType,
     get_feature_dict,
+    get_np_type,
+    get_sa_type,
 )
 
-_GLOBAL_CLS_MAP = {}
+logger = get_logger("atoms")
+
+_GLOBAL_CLS_MAP: dict[type, "_AtomBase"] = {}
+T = TypeVar("T")
 
 
 @dataclass
@@ -45,7 +56,7 @@ ALL_FEATURE_TYPES = ANY_FEATURE_TYPE.__args__
 
 class _AtomBase:
     @classmethod
-    def from_cls(cls, ds_cls):
+    def from_cls(cls: type[T], ds_cls) -> T:
         inst = _GLOBAL_CLS_MAP.get(ds_cls)
         if inst is None:
             inst = cls(name=ds_cls.__name__, description=ds_cls.__doc__)
@@ -79,12 +90,104 @@ class EntityClass(_AtomBase):
     parents: List["EntityClass"] = field(default_factory=list)
     description: Optional[str] = None
 
+    @property
+    def table_index_dt_map(self):
+        return to_dt_map(self.identifiers)
+
+    @property
+    def table_feature_dt_map(self):
+        return to_dt_map(self.properties)
+
+    @property
+    def table_full_dt_map(self):
+        return self.table_index_dt_map | self.table_feature_dt_map
+
+    @property
+    def table_index_cols(self):
+        return list(self.table_index_dt_map.keys())
+
+    @property
+    def table_feature_cols(self):
+        return list(self.table_feature_dt_map.keys())
+
+    @property
+    def table_all_columns(self):
+        return self.table_index_cols + self.table_feature_cols
+
     def _extend(self, ids, props, ds_cls):
         self.parents = [
             p for p in get_simplified_mro(ds_cls) if p is not AbstractEntity
         ]
         self.identifiers = ids
         self.properties = props
+
+
+@dataclass
+class Column:
+    name: str
+    dtype: PrimitiveType
+    nullable: bool = False
+
+
+def feats_to_cols(feats, proc_fk=None, wrap=lambda x: x) -> list[Column]:
+    return chainmap(partial(feat_to_cols, proc_fk=proc_fk, wrap=wrap), feats)
+
+
+def feat_to_cols(feat, proc_fk, wrap, init_prefix=(), open_to_fk=True) -> list:
+
+    new_open_to_fk = True
+    fk_to = None
+    if isinstance(feat, PrimitiveFeature):
+        name = PREFIX_SEP.join([*init_prefix, feat.name])
+        return [wrap(Column(name, feat.dtype, feat.nullable))]
+
+    new_feat_prefix = (*init_prefix, feat.prefix)
+    if isinstance(feat, CompositeFeature):
+        subfeats = feat.dtype.features
+    elif isinstance(feat, ObjectProperty):
+        new_open_to_fk = False
+        fk_to = feat.target
+        subfeats = fk_to.identifiers
+
+    new_fun = partial(
+        feat_to_cols,
+        init_prefix=new_feat_prefix,
+        open_to_fk=new_open_to_fk,
+        proc_fk=proc_fk,
+        wrap=wrap,
+    )
+    out = chainmap(new_fun, subfeats)
+    if fk_to is not None and open_to_fk and proc_fk:
+        proc_fk(out, fk_to, new_feat_prefix)
+    return out
+
+
+def to_dt_map(feats):
+    return {c.name: get_np_type(c.dtype, c.nullable) for c in feats_to_cols(feats)}
+
+
+def to_sa_col(col: Column, pk=False):
+    sa_dt = get_sa_type(col.dtype)
+    return sa.Column(col.name, sa_dt, nullable=col.nullable, primary_key=pk)
+
+
+def parse_df(df: pd.DataFrame, entity: AbstractEntity, verbose=False):
+    entity_class = EntityClass.from_cls(entity)
+    eventual_dic = to_dt_map(entity_class.properties)
+    feat_list = list(eventual_dic.keys())
+    ind_dic = to_dt_map(entity_class.identifiers)
+    set_ind = ind_dic and (set(df.index.names) != set(ind_dic.keys()))
+    if set_ind:
+        if verbose:
+            logger.info("indexing needed", inds=ind_dic)
+        eventual_dic.update(ind_dic)
+
+    missing_cols = set(eventual_dic.keys()) - set(df.columns)
+    if missing_cols:
+        logger.warning(f"missing from columns {missing_cols}", present=df.columns)
+    out = df.astype(eventual_dic)
+    indexed_out = out.set_index(list(ind_dic.keys())) if set_ind else out
+    return indexed_out.loc[:, list(feat_list)]
 
 
 def _ds_cls_to_feat_dicts(ds_cls: Union[EntityClass, CompositeType]):
--- datazimmer/metadata/scrutable.py
+++ datazimmer/metadata/scrutable.py
@@ -1,20 +1,16 @@
 from contextlib import contextmanager
-from dataclasses import dataclass
-from functools import partial
 from typing import Callable, Optional, TypeVar
 
 import pandas as pd
-import sqlalchemy as sa
-from colassigner.constants import PREFIX_SEP
 from colassigner.meta_base import ColMeta
 from structlog import get_logger
 
 from ..config_loading import Config, RunConfig, UnavailableTrepo
 from ..exceptions import ProjectRuntimeException
-from ..utils import camel_to_snake, chainmap, get_creation_module_name
-from .atoms import CompositeFeature, EntityClass, ObjectProperty, PrimitiveFeature
+from ..utils import camel_to_snake, get_creation_module_name
+from .atoms import EntityClass, parse_df
 from .complete_id import CompleteId, CompleteIdBase
-from .datascript import AbstractEntity, PrimitiveType, get_np_type, get_sa_type
+from .datascript import AbstractEntity
 
 logger = get_logger()
 
@@ -35,24 +31,24 @@ class ScruTable:
         self.__module__ = get_creation_module_name()
         self.id_: CompleteId = self._infer_id(entity, self.__module__)
         self.key_map = _parse_entity_map(entity_key_table_map)
-        self.entity_class: EntityClass = EntityClass.from_cls(entity)
+        self.abstract_entity = entity
+        self.entity_class = EntityClass.from_cls(entity)
 
         self.name = self.id_.obj_id
         self.index = self.entity_class.identifiers
         self.features = self.entity_class.properties
-        self.index_map = to_dt_map(self.index)
-        self.features_map = to_dt_map(self.features)
-        self.dtype_map = {**self.index_map, **self.features_map}
-        self.index_cols = [*self.index_map.keys()]
-        self.feature_cols = [*self.features_map.keys()]
-        self.all_cols = self.index_cols + self.feature_cols
+
+        self.index_map = self.entity_class.table_index_dt_map
+        self.features_map = self.entity_class.table_feature_dt_map
+        self.dtype_map = self.entity_class.table_full_dt_map
+        self.index_cols = self.entity_class.table_index_cols
+        self.feature_cols = self.entity_class.table_feature_cols
+        self.all_cols = self.entity_class.table_all_columns
 
         self.partitioning_cols = partitioning_cols
         self.max_partition_size = max_partition_size
         self.trepo = self._conf.create_trepo(
-            self.id_,
-            self.partitioning_cols,
-            self.max_partition_size,
+            self.id_, self.partitioning_cols, self.max_partition_size
         )
         self.get_full_df = self._read_wrap(self.trepo.get_full_df)
         self.map_partitions = self._read_wrap(self.trepo.map_partitions)
@@ -111,72 +107,12 @@ class ScruTable:
     def _parse_df(self, df: pd.DataFrame, verbose=True):
         if verbose:
             logger.info("parsing", table=self.name, namespace=self.id_.namespace)
-        full_dic = self.features_map.copy()
-        set_ind = self.index_map and (set(df.index.names) != set(self.index_cols))
-        if set_ind:
-            if verbose:
-                logger.info("indexing needed", inds=self.index_cols)
-            full_dic.update(self.index_map)
 
-        missing_cols = set(full_dic.keys()) - set(df.columns)
-        if missing_cols:
-            logger.warning(f"missing from columns {missing_cols}", present=df.columns)
-        out = df.astype(full_dic)
-        indexed_out = out.set_index(self.index_cols) if set_ind else out
-        return indexed_out.loc[:, self.feature_cols]
+        return parse_df(df, self.abstract_entity, verbose)
 
-    def _infer_id(self, entity_cls, initing_module_name) -> str:
+    def _infer_id(self, entity_cls, initing_module_name):
         id_base = CompleteIdBase.from_module_name(initing_module_name, self._conf.name)
         return id_base.to_id(camel_to_snake(entity_cls.__name__))
-
-
-@dataclass
-class Column:
-    name: str
-    dtype: PrimitiveType
-    nullable: bool = False
-
-
-def feats_to_cols(feats, proc_fk=None, wrap=lambda x: x) -> list[Column]:
-    return chainmap(partial(feat_to_cols, proc_fk=proc_fk, wrap=wrap), feats)
-
-
-def feat_to_cols(feat, proc_fk, wrap, init_prefix=(), open_to_fk=True) -> list:
-
-    new_open_to_fk = True
-    fk_to = None
-    if isinstance(feat, PrimitiveFeature):
-        name = PREFIX_SEP.join([*init_prefix, feat.name])
-        return [wrap(Column(name, feat.dtype, feat.nullable))]
-
-    new_feat_prefix = (*init_prefix, feat.prefix)
-    if isinstance(feat, CompositeFeature):
-        subfeats = feat.dtype.features
-    elif isinstance(feat, ObjectProperty):
-        new_open_to_fk = False
-        fk_to = feat.target
-        subfeats = fk_to.identifiers
-
-    new_fun = partial(
-        feat_to_cols,
-        init_prefix=new_feat_prefix,
-        open_to_fk=new_open_to_fk,
-        proc_fk=proc_fk,
-        wrap=wrap,
-    )
-    out = chainmap(new_fun, subfeats)
-    if fk_to is not None and open_to_fk and proc_fk:
-        proc_fk(out, fk_to, new_feat_prefix)
-    return out
-
-
-def to_dt_map(feats):
-    return {c.name: get_np_type(c.dtype, c.nullable) for c in feats_to_cols(feats)}
-
-
-def to_sa_col(col: Column, pk=False):
-    sa_dt = get_sa_type(col.dtype)
-    return sa.Column(col.name, sa_dt, nullable=col.nullable, primary_key=pk)
 
 
 def _parse_entity_map(entity_map: dict):
